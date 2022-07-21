bcb6b5abf41be9d5b776a89e5cc5ae23e1de1004
olart <akanbi.ges201@gmail.com>
2022-07-21 20:16:56 +0100

    made PII_FIELDS string and not list

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ from typing import Union, List
 import re
 
 
-PII_FIELDS = (['name', 'email', 'phone', 'ssn', 'password'])
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
