54c42b68fbb0a5344ac3d4ea85e6f5c0cec91348
olart <akanbi.ges201@gmail.com>
2022-07-25 23:35:23 +0100

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""main file for api authentication"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """class to manage api authentication"""
+
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool:
+        """returns a bool based on path and excluded_paths"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """returns the request status"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns the request status object"""
+        return None
