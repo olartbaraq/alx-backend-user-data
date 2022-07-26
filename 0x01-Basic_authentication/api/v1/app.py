410e6ecb6b062ffc4f5b67bac2a6ffe2b273a458
olart <akanbi.ges201@gmail.com>
2022-07-26 13:30:06 +0100

    6.Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,11 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = os.getenv('AUTH_TYPE')
-if auth:
+basic_auth = os.getenv('AUTH_TYPE')
+if basic_auth:
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""text file class basicAuth that
+inherits from base class"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """class basicAuth that inherits from base class"""
+    pass
