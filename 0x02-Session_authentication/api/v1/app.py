3c34178d9ea34d6608bbbae3a94dfa5e26da269f
olart <akanbi.ges201@gmail.com>
2022-07-27 23:21:27 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,10 +56,12 @@ def filter_request():
         requires_auth = auth.require_auth(request.path, [
                                             '/api/v1/status/',
                                             '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/'
+                                            '/api/v1/forbidden/',
+                                            '/api/v1/auth_session/login/'
                                         ])
         if requires_auth:
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request) and\
+               not auth.session_cookie(request):
                 return abort(401)
             if not auth.current_user(request):
                 return abort(403)
