ab93aeee652285ad15a55ca71f5012509a9e0dad
olart <akanbi.ges201@gmail.com>
2022-08-02 20:04:55 +0100

    task 0-3

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,10 @@
+<p>In the industry, you should not implement your own authentication system and use a module or framework that doing it for you (like in Python-Flask: Flask-User). Here, for the learning purpose, we will walk through each step of this mechanism to understand it by doing.</p>
+
+<p>At the end of this project, you are expected to be able to explain to anyone, <b>without the help of Google</b>:</p>
+
+<ul>
+<li>How to declare API routes in a Flask app<li>
+<li>How to get and set cookies<li>
+<li>How to retrieve request form data<li>
+<li>How to return various HTTP status codes<li>
+</ul>
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+db file to implement add_user method
+"""
+
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ saves the user to the database"""
+        new_user = User(email=email,
+                        hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ find user by some arguments
+        Returns:
+            User: user found or raise error
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        elif kwargs is None:
+            raise InvalidRequestError
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ locate the user to update """
+        user = self.find_user_by(id=user_id)
+        for key, val in kwargs.items():
+            if key not in DATA:
+                raise ValueError
+            setattr(user, key, val)
+        self._session.commit()
+        return None
