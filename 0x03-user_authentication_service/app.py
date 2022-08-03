aec3129ef95d8defafa8135bb3951667ca6c4e5b
olart <akanbi.ges201@gmail.com>
2022-08-03 18:05:24 +0100

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 
 """basic flask app"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 
@@ -28,6 +28,18 @@ def users() -> str:
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """returns the JSON payload of logged in user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid_login = AUTH.valid_login(email, password)
+    if not valid_login:
+        abort(401)
+    session_id = AUTH.create_session(email=email)
+    resp = jsonify({"email": f'{email}', "message": "logged in"})
+    resp.set_cookie('session_id', session_id)
+    return resp
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
